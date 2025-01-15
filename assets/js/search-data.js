d0634a39143415448743f03e1c3ebefc66439c35
ringku09 <ringku_740@yahoo.com>
2025-01-15 07:36:14 +0000

    Deploying to gh-pages from @ Fun-Gene/fun-gene.github.io@9d3d40ae9390ed20a1882de7b846c23623c4a0c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "Team and alumni",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-code",
           title: "code",
           description: "We aim to open-source our research to foster transparency, collaboration, and reproducibility.",
@@ -414,51 +407,31 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-md-masud-rana",
+          title: 'Md. Masud Rana',
+          description: "MSc student",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/ringku_msc/";
+            },},{id: "projects-md-masud-rana",
+          title: 'Md. Masud Rana',
+          description: "MSc student",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/ringku_msc_alm/";
+            },},{id: "projects-md-masud-rana",
+          title: 'Md. Masud Rana',
+          description: "PhD student",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/ringku_phd/";
+            },},{id: "projects-md-masud-rana",
+          title: 'Md. Masud Rana',
+          description: "PhD student",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/ringku_phd_alm/";
+            },},{id: "projects-tao-xiangming",
+          title: 'Tao Xiangming',
+          description: "PhD student",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/tao_phd_alm/";
             },},{
         id: 'social-email',
         title: 'email',
