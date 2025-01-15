d057a7e97e765625e80d245442242ee9a297e4ad
ringku09 <ringku_740@yahoo.com>
2025-01-15 19:30:34 +0000

    Deploying to gh-pages from @ Fun-Gene/fun-gene.github.io@292f85bcb39094b9add93a3873681ad04c07bd40 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,16 +403,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-happy-new-year-sparkles-smile",
+          title: 'Happy new year! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-md-masud-rana",
           title: 'Md. Masud Rana',
