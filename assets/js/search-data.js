4c747d9047a8f329504ef6f18984a6abdfaa763e
ringku09 <ringku_740@yahoo.com>
2025-01-16 06:38:42 +0000

    Deploying to gh-pages from @ Fun-Gene/fun-gene.github.io@f1a8e6c142d65ed80231a1f8f3dece25b8ad39dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Team and alumni",
+          description: "Teaching materials",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
